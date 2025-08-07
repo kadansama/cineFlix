3976dbc7aefa354fc0059a763e5c55e1904eb6dc
AidarProgrammer <aidarchess27@gmail.com>
2025-08-07 12:43:59 +0300

    WIP: правки в PopularFilmText

--- src/features/popularFilms/components/PopularFilmText/PopularFilmText.tsx
+++ src/features/popularFilms/components/PopularFilmText/PopularFilmText.tsx
@@ -9,11 +9,11 @@ type PopularFilmTextBlockProps = {
 };
 
 const blockStyles: Record<BlockType, { variant: "h1" | "h2" | "h5" | "h3" | "h6" | "body1" | "body2", sx?: object }> = {
-  label:   { variant: "h5", sx: { fontWeight: 500, marginBottom: 1 } },
+  label:   { variant: "h5", sx: { fontWeight: 700, marginBottom: 1, borderLeft: '4px solid var(--red)', paddingLeft: '10px'} },
   title:   { variant: "h2", sx: { fontWeight: "bold", marginBottom: 2 } },
   rating:  { variant: "h5", sx: {marginBottom: 1 } },
   overview:{ variant: "h6", sx: {marginBottom: 2, maxWidth: "40%"} },
-  genres:  { variant: "h5"},
+  genres:  { variant: "h6"},
 };
 
 export const PopularFilmTextBlock = ({ type, content }: PopularFilmTextBlockProps): ReactElement => {
